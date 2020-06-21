aa6a80b2ecd7f19256b69bd1f7940edf599103af
AlkVo <halfsteamedbun@gmail.com>
2020-06-21 17:18:36 +0800

    component 和 ThingState 连接

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import AddThingForm from './components/things/AddThingForm';
 import ToDoTings from './components/things/ToDoTings';
 import CurrentThing from './components/things/CurrentThing';
 import HistoryThings from './components/things/HistoryThings';
+import ThingState from './context/things/ThingState';
 
 function App() {
   return (
-    <div className='App'>
-      添加表单
-      <AddThingForm />
-      <br />
-      当前要做
-      <CurrentThing />
-      待办列表
-      <ToDoTings />
-      历史
-      <HistoryThings />
-    </div>
+    <ThingState>
+      <div className='App'>
+        添加表单
+        <AddThingForm />
+        <br />
+        当前要做
+        <CurrentThing />
+        待办列表
+        <ToDoTings />
+        历史
+        <HistoryThings />
+      </div>
+    </ThingState>
   );
 }
 
